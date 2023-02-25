dfeaff8b49b8b5fa1aba6e76788e76db77980daf
Ferhat Savtak <ferhatsavtak@gmail.com>
2023-02-25 23:52:03 +0300

    Project Added

---
+++ WindowsFormsApplication5/Form1.Designer.cs
@@ -0,0 +1,420 @@
+﻿namespace WindowsFormsApplication5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.yaricapaScrollBar = new System.Windows.Forms.HScrollBar();
+            this.hScrollBar2 = new System.Windows.Forms.HScrollBar();
+            this.yaricapbScrollBar = new System.Windows.Forms.HScrollBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.yaricapcScrollBar = new System.Windows.Forms.HScrollBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.hiz = new System.Windows.Forms.TextBox();
+            this.h = new System.Windows.Forms.TextBox();
+            this.yuk = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            this.label21 = new System.Windows.Forms.Label();
+            this.label22 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // yaricapaScrollBar
+            // 
+            this.yaricapaScrollBar.Location = new System.Drawing.Point(566, 63);
+            this.yaricapaScrollBar.Maximum = 200;
+            this.yaricapaScrollBar.Name = "yaricapaScrollBar";
+            this.yaricapaScrollBar.Size = new System.Drawing.Size(172, 19);
+            this.yaricapaScrollBar.TabIndex = 0;
+            this.yaricapaScrollBar.Value = 20;
+            this.yaricapaScrollBar.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar1_Scroll);
+            // 
+            // hScrollBar2
+            // 
+            this.hScrollBar2.Location = new System.Drawing.Point(63, 238);
+            this.hScrollBar2.Maximum = 500;
+            this.hScrollBar2.Name = "hScrollBar2";
+            this.hScrollBar2.Size = new System.Drawing.Size(172, 20);
+            this.hScrollBar2.TabIndex = 1;
+            this.hScrollBar2.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar2_Scroll);
+            // 
+            // yaricapbScrollBar
+            // 
+            this.yaricapbScrollBar.Location = new System.Drawing.Point(566, 104);
+            this.yaricapbScrollBar.Maximum = 300;
+            this.yaricapbScrollBar.Name = "yaricapbScrollBar";
+            this.yaricapbScrollBar.Size = new System.Drawing.Size(172, 23);
+            this.yaricapbScrollBar.TabIndex = 2;
+            this.yaricapbScrollBar.Value = 150;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(408, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(357, 181);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // yaricapcScrollBar
+            // 
+            this.yaricapcScrollBar.Location = new System.Drawing.Point(566, 154);
+            this.yaricapcScrollBar.Maximum = 250;
+            this.yaricapcScrollBar.Name = "yaricapcScrollBar";
+            this.yaricapcScrollBar.Size = new System.Drawing.Size(172, 20);
+            this.yaricapcScrollBar.TabIndex = 5;
+            this.yaricapcScrollBar.Value = 50;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(753, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(326, 281);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(753, 161);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(753, 114);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "label5";
+            // 
+            // hiz
+            // 
+            this.hiz.Location = new System.Drawing.Point(98, 62);
+            this.hiz.Multiline = true;
+            this.hiz.Name = "hiz";
+            this.hiz.Size = new System.Drawing.Size(169, 20);
+            this.hiz.TabIndex = 10;
+            // 
+            // h
+            // 
+            this.h.Location = new System.Drawing.Point(98, 114);
+            this.h.Name = "h";
+            this.h.Size = new System.Drawing.Size(169, 20);
+            this.h.TabIndex = 11;
+            // 
+            // yuk
+            // 
+            this.yuk.Location = new System.Drawing.Point(98, 158);
+            this.yuk.Name = "yuk";
+            this.yuk.Size = new System.Drawing.Size(169, 20);
+            this.yuk.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(753, 181);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(753, 220);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(762, 245);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(483, 69);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(55, 13);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "A Kasnağı";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label10.Location = new System.Drawing.Point(618, 8);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(80, 20);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "Yarıçaplar";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(483, 114);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(55, 13);
+            this.label11.TabIndex = 18;
+            this.label11.Text = "B Kasnağı";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(483, 161);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(55, 13);
+            this.label12.TabIndex = 19;
+            this.label12.Text = "C Kasnağı";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(794, 69);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(23, 13);
+            this.label13.TabIndex = 20;
+            this.label13.Text = "mm";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(794, 114);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(23, 13);
+            this.label14.TabIndex = 21;
+            this.label14.Text = "mm";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(794, 161);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(23, 13);
+            this.label15.TabIndex = 22;
+            this.label15.Text = "mm";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label16.Location = new System.Drawing.Point(121, 9);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(58, 20);
+            this.label16.TabIndex = 23;
+            this.label16.Text = "Girişler";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(22, 68);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(22, 13);
+            this.label17.TabIndex = 24;
+            this.label17.Text = "Hız";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(25, 120);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(53, 13);
+            this.label18.TabIndex = 25;
+            this.label18.Text = "Yükseklik";
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Location = new System.Drawing.Point(25, 161);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(26, 13);
+            this.label19.TabIndex = 26;
+            this.label19.Text = "Yük";
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.Location = new System.Drawing.Point(288, 68);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(25, 13);
+            this.label20.TabIndex = 27;
+            this.label20.Text = "m/s";
+            // 
+            // label21
+            // 
+            this.label21.AutoSize = true;
+            this.label21.Location = new System.Drawing.Point(291, 120);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(15, 13);
+            this.label21.TabIndex = 28;
+            this.label21.Text = "m";
+            // 
+            // label22
+            // 
+            this.label22.AutoSize = true;
+            this.label22.Location = new System.Drawing.Point(291, 161);
+            this.label22.Name = "label22";
+            this.label22.Size = new System.Drawing.Size(19, 13);
+            this.label22.TabIndex = 29;
+            this.label22.Text = "kg";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(863, 594);
+            this.Controls.Add(this.label22);
+            this.Controls.Add(this.label21);
+            this.Controls.Add(this.label20);
+            this.Controls.Add(this.label19);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.yuk);
+            this.Controls.Add(this.h);
+            this.Controls.Add(this.hiz);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.yaricapcScrollBar);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.yaricapbScrollBar);
+            this.Controls.Add(this.hScrollBar2);
+            this.Controls.Add(this.yaricapaScrollBar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.HScrollBar yaricapaScrollBar;
+        private System.Windows.Forms.HScrollBar hScrollBar2;
+        private System.Windows.Forms.HScrollBar yaricapbScrollBar;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.HScrollBar yaricapcScrollBar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox hiz;
+        private System.Windows.Forms.TextBox h;
+        private System.Windows.Forms.TextBox yuk;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.Label label21;
+        private System.Windows.Forms.Label label22;
+    }
+}
+
---
+++ WindowsFormsApplication5/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication5
+{
+    public partial class Form1 : Form
+    
+        
+    
+    {
+        double   x1,x2, yaricapa=100d, aradakimesafe=250d;  
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint, 
+            true); this.SetStyle(ControlStyles.OptimizedDoubleBuffer, 
+            true); yaricapaScrollBar.Value = Convert.ToInt32(yaricapa); 
+            hScrollBar2.Value = Convert.ToInt32(aradakimesafe); 
+        }
+
+        private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            yaricapa = Convert.ToDouble(yaricapaScrollBar.Value);
+        }
+
+        private void hScrollBar2_Scroll(object sender, ScrollEventArgs e)
+        {
+            aradakimesafe = Convert.ToDouble(hScrollBar2.Value); 
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           
+            label2.Text = Convert.ToString(yaricapaScrollBar.Value*2);
+            label3.Text = Convert.ToString(hScrollBar2.Value);
+            label4.Text = Convert.ToString(yaricapcScrollBar.Value*2);
+            label5.Text = Convert.ToString(yaricapbScrollBar.Value*2);
+            
+            
+            Invalidate();
+        }
+            private float c(double a)
+        { 
+            
+            
+         return Convert.ToSingle(a);
+        
+        }  
+    
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+           ///int c(double( x))
+           
+            
+            Pen p = new Pen(Color.Coral, 5);  
+            e.Graphics.TranslateTransform(30,30);  
+            //teta += 0.105; // x = L * Math.Cos(teta)*0;  
+           // y = L * Math.Sin(teta)*0;  
+            //e.Graphics.DrawLine(p,0,0,c(x),c(y));              
+            x1 =  Math.Sqrt(aradakimesafe*aradakimesafe);
+            //x2 =  Math.Sqrt(aradakimesafe * aradakimesafe);
+            e.Graphics.DrawLine(p, yaricapaScrollBar.Value/2, 0, c(x1), 0);
+            e.Graphics.DrawLine(p, yaricapaScrollBar.Value / 2, yaricapaScrollBar.Value, c(x1), yaricapbScrollBar.Value);
+            e.Graphics.DrawEllipse(p, 0, 0, yaricapaScrollBar.Value, yaricapaScrollBar.Value);
+            e.Graphics.DrawEllipse(p, c(x1 -yaricapbScrollBar.Value/2), 0, yaricapbScrollBar.Value, yaricapbScrollBar.Value);
+            e.Graphics.DrawEllipse(p, c(x1 - yaricapcScrollBar.Value / 2), yaricapbScrollBar.Value / 2 - yaricapcScrollBar.Value / 2, yaricapcScrollBar.Value, yaricapcScrollBar.Value);
+            e.Graphics.DrawLine(p, c(x1) + yaricapcScrollBar.Value / 2, yaricapbScrollBar.Value / 2, c(x1) + yaricapcScrollBar.Value / 2, 200);
+            e.Graphics.DrawRectangle(p, c(x1) + yaricapcScrollBar.Value / 2-15, 200, 30, 30);
+        }
+        
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            
+            double g= 9.81;
+            double hizz=Convert.ToDouble(hiz.Text);
+            double yukk = Convert.ToDouble(yuk.Text);
+            double hh = Convert.ToDouble(h.Text);
+            double rc = Convert.ToDouble(label4.Text);
+            double rb = Convert.ToDouble(label5.Text);
+            double ra=Convert.ToDouble(label2.Text);
+
+
+            double a = (hizz*hizz) / (2 * hh);
+            double Fc = yukk * (g + a);
+            double Mc = Fc * (rc/1000);
+            double Mb = Mc;
+            double Fb = Mb/(rb/1000);
+            double Fa=Fb;
+            double Ma=Fa*(ra/1000);
+            double Wc = ((hizz) / (rc / 1000));
+            double Wb = Wc;
+            double vb = (Wb * (rb / 1000));
+            double va = vb;
+            double Wa = (va / (ra / 1000));
+            double n = (Wa * 30) / 3.14159265358979323846;
+            double P = Ma * Wa;
+            double sonP = P * 1.25;
+
+            double kayisboyu = 2 * (2 * ra + 2 * rb) + (3.14159265358979323846 / 2) * (2 * ra + 2 * rb) + (2 * rb - 2 * ra) * (2 * rb - 2 * ra) / 4 * (2 * ra + 2 * rb);
+            
+            label6.Text = Convert.ToString(sonP);
+            label7.Text = Convert.ToString(kayisboyu);
+
+            double yukemniyet = yukk * 5;
+
+            if (yukemniyet < 1990)
+                label8.Text = "6 mm";
+            else if (yukemniyet < 3540)
+                label8.Text = "8 mm";
+            else if (yukemniyet < 5350)
+                label8.Text = "10 mm";
+            else if (yukemniyet < 7970)
+                label8.Text = "12 mm";
+            else if (yukemniyet < 9350)
+                label8.Text = "13 mm";
+            else if (yukemniyet < 10800)
+                label8.Text = "14 mm";
+            else if (yukemniyet < 14200)
+                label8.Text = "16 mm";
+            else if (yukemniyet < 17900)
+                label8.Text = "18 mm";
+            else if (yukemniyet < 19950)
+                label8.Text = "19 mm";
+            else if (yukemniyet < 22100)
+                label8.Text = "20 mm";
+            else if (yukemniyet < 26800)
+                label8.Text = "22 mm";
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+       
+        
+    }
+}
